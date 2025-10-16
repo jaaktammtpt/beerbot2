fe5cc596e96344804ce8c242e2be493e66ba982c
jaaktammtpt <47696558+jaaktammtpt@users.noreply.github.com>
2025-10-16 23:04:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ from fastapi import FastAPI, Request
 # mis on optimeeritud kulude minimeerimiseks. Need on kõige olulisemad parameetrid.
 # Madalam Q-väärtus jaemüüjal peegeldab kiiremat reageerimist kliendi nõudlusele.
 OPTIMIZED_Q_VALUES = {
-    "retailer": 1.5,
+    "retailer": 2.5,
     "wholesaler": 2.5,
-    "distributor": 2.5,
+    "distributor": 3.5,
     "factory": 4.5,
 }
 
 # Periood (nädalates) liikuva keskmise arvutamiseks nõudluse prognoosimisel.
-MOVING_AVERAGE_PERIOD = 4
+MOVING_AVERAGE_PERIOD = 5
 
 # Silumisperiood (nädalates) mängu alguses, et vältida järske tellimusi.
 SMOOTHING_PERIOD = 5
