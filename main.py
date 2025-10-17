82d2c58442f532f439dfc5924182e53a00c9a408
jaaktammtpt <47696558+jaaktammtpt@users.noreply.github.com>
2025-10-18 00:49:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from fastapi import FastAPI, Request
 OPTIMIZED_Q_VALUES = {
     "retailer": 1.8, #1.80
     "wholesaler": 3.1, #3.1 
-    "distributor": 4.85, #4.75
+    "distributor": 4.9, #4.75
     "factory": 2.0, # 2.0  Tehas vajab veidi rohkem pikema tootmistsükli tõttu
 }
 
