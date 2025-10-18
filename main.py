153853d3c81446f1bb87a71ae5b6f3859a754824
jaaktammtpt <47696558+jaaktammtpt@users.noreply.github.com>
2025-10-18 23:10:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ Q_GLASSBOX = {
 }
 
 Q_BLACKBOX = {
-    "retailer": 2.4,
-    "wholesaler": 3.0, # 3.0
+    "retailer": 2.4, # 2.3
+    "wholesaler": 3.1, # 3.0
     "distributor": 4.3, # 4.2
     "factory": 2.5
 }
